5baa745b8c03938350376d34c92e0407e86a8765
pqnpetit <pqnpetit@gmail.com>
2023-07-18 01:54:43 -0300

    v1.10.3 add etapa.js

---
+++ .vscode/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'../Imagens/1475137.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'../Imagens/2303790.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'../Imagens/4470321.png', legenda: 'Prefeito'},
+                    {url:'../Imagens/4151385.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'../Imagens/2303842.png', legenda: 'Prefeito'},
+                    {url:'../Imagens/1475137.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
